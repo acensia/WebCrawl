a8754e03142f5837c7ac67d786adeb140dcd0d2d
acensia <acensia@snu.ac.kr>
2023-09-05 23:47:01 +0900

    add comments

--- get_img.py
+++ get_img.py
@@ -9,32 +9,29 @@ import json
 
 
 def get_imgs(driver, save_path):
+    # get the image of the set
     image_element = driver.find_elements(By.XPATH, '//div[@class="codimap-cont"]/img')
-
     image_url = image_element[0].get_attribute('src')
     response = requests.get(image_url)
     with open(f'{save_path}/set_image.jpg', 'wb') as file:
         file.write(response.content)
-    # Close the driver
 
-    image_singles = driver.find_elements(By.CSS_SELECTOR, '.swiper-slide.style_contents_size')
-    
-    img_single = driver.find_elements(By.XPATH, '//a[@class="styling_img"]/img')
-    brand_single = driver.find_elements(By.XPATH, '//a[@class="brand"]')
-    brand_items = driver.find_elements(By.XPATH, '//a[@class="brand_item"]')
-    price_items = driver.find_elements(By.XPATH, '//div[@class="price"]')
+    img_single = driver.find_elements(By.XPATH, '//a[@class="styling_img"]/img') # get the images of each cloth constructing the set
+    brand_single = driver.find_elements(By.XPATH, '//a[@class="brand"]') # get the brand of each cloth
+    brand_items = driver.find_elements(By.XPATH, '//a[@class="brand_item"]') # get the name of each cloth
+    price_items = driver.find_elements(By.XPATH, '//div[@class="price"]') # get the price of each cloth (not completed)
     
     anno = {}
-    anno["set_name"] = driver.find_element(By.XPATH, '//div[@class="styling_content codimap-contents"]/h2').text
+    anno["set_name"] = driver.find_element(By.XPATH, '//div[@class="styling_content codimap-contents"]/h2').text # get the name of the set
     items = []
     
     for i, s in enumerate(img_single):
-        img_url = s.get_attribute('src')
+        img_url = s.get_attribute('src') # single image
         res = requests.get(img_url)
         with open(f"{save_path}/img{i}.jpg", 'wb') as f:
             f.write(res.content)
         
-        item = {}
+        item = {} # annotation info
         item["brand"] = brand_single[i].text
         item["item"] = brand_items[i].text
         item["curr_price"] = price_items[i].text
--- real_crawl.py
+++ real_crawl.py
@@ -7,9 +7,12 @@ import time
 import requests as rq
 import os
 from get_img import get_imgs
+import re
 
-
+# target styles
 styles = ["casual", "formal", "sports", "street", "gorpcore", "dandy"]
+
+
 def crawl_style(style, base_path):
     
     # Initialize the driver
@@ -18,21 +21,41 @@ def crawl_style(style, base_path):
     os.makedirs(f"{base_path}/{style}", exist_ok=True)
     os.makedirs(f"{base_path}/{style}/male", exist_ok=True)
     os.makedirs(f"{base_path}/{style}/female", exist_ok=True)
+    
     page = 1
     total_sets = 0
-    while total_sets < 200:
+    limit = 200
+    
+    # check limitation of the number of cloth sets for male
+    # 1. get into male catalog
+    driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
+    btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
+    driver.execute_script("arguments[0].click();", btns[1])
+    # 2. get total nums of sets
+    cnt = driver.find_element(By.XPATH, '//span[@class="counter"]')
+    num_text = cnt.text
+    match = re.search(r'(\d{1,3}(?:,\d{3})*)', num_text)
+    num_str = match.group(1)
+    num = int(num_str.replace(',', ''))
+    
+    if num < limit:
+        limit = num
+        
+            
+    while total_sets < limit:
+        # get into page numbered as "page" value
         driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
         btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
         driver.execute_script("arguments[0].click();", btns[1])
-        
+    
+        # get all of thumnail elements in catalog
         image_thumbnails = driver.find_elements(By.XPATH, '//div[@class="style-list-thumbnail"]/img')
         for th in image_thumbnails:
-            print(f"{style} : {total_sets}")
+            print(f"{style} : {total_sets}") # just for monitoring
             os.makedirs(f"{base_path}/{style}/male/{total_sets}", exist_ok=True)
-            driver.execute_script("arguments[0].click();", th)
+            driver.execute_script("arguments[0].click();", th) # get into the page of each set clicking the thumbnail
             time.sleep(0.3)
-            get_imgs(driver, f"{base_path}/{style}/male/{total_sets}")
-            
+            get_imgs(driver, f"{base_path}/{style}/male/{total_sets}") # call module
             driver.back()
             time.sleep(0.3)
             total_sets += 1
@@ -43,12 +66,30 @@ def crawl_style(style, base_path):
 
     page = 1
     total_sets = 0        
+    limit = 200
     
-    while total_sets < 200:
+    # check limitation of the number of cloth sets for female
+    # 1. get into female catalog
+    driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
+    btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
+    driver.execute_script("arguments[0].click();", btns[2])
+    # 2. get total nums of sets
+    cnt = driver.find_element(By.XPATH, '//span[@class="counter"]')
+    num_text = cnt.text
+    match = re.search(r'(\d{1,3}(?:,\d{3})*)', num_text)
+    num_str = match.group(1)
+    num = int(num_str.replace(',', ''))
+    
+    if num < limit:
+        limit = num
+
+    
+    
+    while total_sets < limit:
         driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
         btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
         driver.execute_script("arguments[0].click();", btns[2])
-        
+                
         image_thumbnails = driver.find_elements(By.XPATH, '//div[@class="style-list-thumbnail"]/img')
         for th in image_thumbnails:
             print(f"{style} : {total_sets}")
