3ca9b8793e2e1e83dbbfc6ca6220194738da75cc
acensia <acensia@snu.ac.kr>
2023-09-06 02:03:20 +0900

    base path error

--- crawl_allset.py
+++ crawl_allset.py
@@ -26,43 +26,43 @@ def crawl_style(style, base_path):
     total_sets = 0
     limit = 200
     
-    # check limitation of the number of cloth sets for male
-    # 1. get into male catalog
-    driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
-    btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
-    driver.execute_script("arguments[0].click();", btns[1])
-    # 2. get total nums of sets
-    cnt = driver.find_element(By.XPATH, '//span[@class="counter"]')
-    num_text = cnt.text
-    match = re.search(r'(\d{1,3}(?:,\d{3})*)', num_text)
-    num_str = match.group(1)
-    num = int(num_str.replace(',', ''))
+    # # check limitation of the number of cloth sets for male
+    # # 1. get into male catalog
+    # driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
+    # btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
+    # driver.execute_script("arguments[0].click();", btns[1])
+    # # 2. get total nums of sets
+    # cnt = driver.find_element(By.XPATH, '//span[@class="counter"]')
+    # num_text = cnt.text
+    # match = re.search(r'(\d{1,3}(?:,\d{3})*)', num_text)
+    # num_str = match.group(1)
+    # num = int(num_str.replace(',', ''))
     
-    if num < limit:
-        limit = num
+    # if num < limit:
+    #     limit = num
         
             
-    while total_sets < limit:
-        # get into page numbered as "page" value
-        driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
-        btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
-        driver.execute_script("arguments[0].click();", btns[1])
+    # while total_sets < limit:
+    #     # get into page numbered as "page" value
+    #     driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
+    #     btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
+    #     driver.execute_script("arguments[0].click();", btns[1])
     
-        # get all of thumnail elements in catalog
-        image_thumbnails = driver.find_elements(By.XPATH, '//div[@class="style-list-thumbnail"]/img')
-        for th in image_thumbnails:
-            print(f"{style} : {total_sets}") # just for monitoring
-            os.makedirs(f"{base_path}/{style}/male/{total_sets}", exist_ok=True)
-            driver.execute_script("arguments[0].click();", th) # get into the page of each set clicking the thumbnail
-            time.sleep(0.3)
-            get_imgs(driver, f"{base_path}/{style}/male/{total_sets}") # call module
-            driver.back()
-            time.sleep(0.3)
-            total_sets += 1
-            if total_sets == 200:
-                break
+    #     # get all of thumnail elements in catalog
+    #     image_thumbnails = driver.find_elements(By.XPATH, '//div[@class="style-list-thumbnail"]/img')
+    #     for th in image_thumbnails:
+    #         print(f"{style} : {total_sets}") # just for monitoring
+    #         os.makedirs(f"{base_path}/{style}/male/{total_sets}", exist_ok=True)
+    #         driver.execute_script("arguments[0].click();", th) # get into the page of each set clicking the thumbnail
+    #         time.sleep(0.3)
+    #         get_imgs(driver, f"{base_path}/{style}/male/{total_sets}") # call module
+    #         driver.back()
+    #         time.sleep(0.3)
+    #         total_sets += 1
+    #         if total_sets == 200:
+    #             break
             
-        page += 1
+    #     page += 1
 
     page = 1
     total_sets = 0        
@@ -82,8 +82,6 @@ def crawl_style(style, base_path):
     
     if num < limit:
         limit = num
-
-    
     
     while total_sets < limit:
         driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
@@ -93,10 +91,10 @@ def crawl_style(style, base_path):
         image_thumbnails = driver.find_elements(By.XPATH, '//div[@class="style-list-thumbnail"]/img')
         for th in image_thumbnails:
             print(f"{style} : {total_sets}")
-            os.makedirs(f"./shown/{style}/female/{total_sets}", exist_ok=True)
+            os.makedirs(f"{base_path}/{style}/female/{total_sets}", exist_ok=True)
             driver.execute_script("arguments[0].click();", th)
             time.sleep(0.3)
-            get_imgs(driver, f"./shown/{style}/female/{total_sets}")
+            get_imgs(driver, f"./{base_path}/{style}/female/{total_sets}")
             
             driver.back()
             time.sleep(0.3)
