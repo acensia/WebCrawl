18e6bba5b0da5d750cd43406d338816f5d40ab31
acensia <acensia@snu.ac.kr>
2023-09-06 02:08:20 +0900

    gender value parameterize

--- crawl_allset.py
+++ crawl_allset.py
@@ -12,67 +12,28 @@ import re
 # target styles
 styles = ["casual", "formal", "sports", "street", "gorpcore", "dandy"]
 
-
-def crawl_style(style, base_path):
+# gender value 
+def crawl_style(style, base_path, gender):
     
     # Initialize the driver
     driver = webdriver.Chrome()
     
     os.makedirs(f"{base_path}/{style}", exist_ok=True)
-    os.makedirs(f"{base_path}/{style}/male", exist_ok=True)
-    os.makedirs(f"{base_path}/{style}/female", exist_ok=True)
+    os.makedirs(f"{base_path}/{style}/{gender}", exist_ok=True)
     
     page = 1
     total_sets = 0
     limit = 200
+    if gender == "male":
+        idx = 1
+    else :
+        idx = 2
     
-    # # check limitation of the number of cloth sets for male
-    # # 1. get into male catalog
-    # driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
-    # btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
-    # driver.execute_script("arguments[0].click();", btns[1])
-    # # 2. get total nums of sets
-    # cnt = driver.find_element(By.XPATH, '//span[@class="counter"]')
-    # num_text = cnt.text
-    # match = re.search(r'(\d{1,3}(?:,\d{3})*)', num_text)
-    # num_str = match.group(1)
-    # num = int(num_str.replace(',', ''))
-    
-    # if num < limit:
-    #     limit = num
-        
-            
-    # while total_sets < limit:
-    #     # get into page numbered as "page" value
-    #     driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
-    #     btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
-    #     driver.execute_script("arguments[0].click();", btns[1])
-    
-    #     # get all of thumnail elements in catalog
-    #     image_thumbnails = driver.find_elements(By.XPATH, '//div[@class="style-list-thumbnail"]/img')
-    #     for th in image_thumbnails:
-    #         print(f"{style} : {total_sets}") # just for monitoring
-    #         os.makedirs(f"{base_path}/{style}/male/{total_sets}", exist_ok=True)
-    #         driver.execute_script("arguments[0].click();", th) # get into the page of each set clicking the thumbnail
-    #         time.sleep(0.3)
-    #         get_imgs(driver, f"{base_path}/{style}/male/{total_sets}") # call module
-    #         driver.back()
-    #         time.sleep(0.3)
-    #         total_sets += 1
-    #         if total_sets == 200:
-    #             break
-            
-    #     page += 1
-
-    page = 1
-    total_sets = 0        
-    limit = 200
-    
-    # check limitation of the number of cloth sets for female
-    # 1. get into female catalog
+    # check limitation of the number of cloth sets for male
+    # 1. get into male catalog
     driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
     btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
-    driver.execute_script("arguments[0].click();", btns[2])
+    driver.execute_script("arguments[0].click();", btns[idx])
     # 2. get total nums of sets
     cnt = driver.find_element(By.XPATH, '//span[@class="counter"]')
     num_text = cnt.text
@@ -81,28 +42,31 @@ def crawl_style(style, base_path):
     num = int(num_str.replace(',', ''))
     
     if num < limit:
+        print("The num of sets is less than limit")
         limit = num
-    
+        
+            
     while total_sets < limit:
+        # get into page numbered as "page" value
         driver.get(f"https://www.musinsa.com/app/codimap/lists?style_type={style}&tag_no=&brand=&display_cnt=60&list_kind=big&sort=date&page={page}")
         btns = driver.find_elements(By.XPATH, '//div[@class="sc-pvystx-0 iRAZYo"]/button')
-        driver.execute_script("arguments[0].click();", btns[2])
-                
+        driver.execute_script("arguments[0].click();", btns[idx])
+    
+        # get all of thumnail elements in catalog
         image_thumbnails = driver.find_elements(By.XPATH, '//div[@class="style-list-thumbnail"]/img')
         for th in image_thumbnails:
-            print(f"{style} : {total_sets}")
-            os.makedirs(f"{base_path}/{style}/female/{total_sets}", exist_ok=True)
-            driver.execute_script("arguments[0].click();", th)
+            print(f"{style} : {total_sets}") # just for monitoring
+            os.makedirs(f"{base_path}/{style}/{gender}/{total_sets}", exist_ok=True)
+            driver.execute_script("arguments[0].click();", th) # get into the page of each set clicking the thumbnail
             time.sleep(0.3)
-            get_imgs(driver, f"./{base_path}/{style}/female/{total_sets}")
-            
+            get_imgs(driver, f"{base_path}/{style}/{gender}/{total_sets}") # call module
             driver.back()
             time.sleep(0.3)
             total_sets += 1
             if total_sets == 200:
                 break
             
-        page += 1  
+        page += 1
         
     driver.quit()
     
@@ -112,5 +76,5 @@ if __name__ == "__main__":
     os.makedirs(base_path, exist_ok=True)
     
     for st in styles:
-        crawl_style(st, base_path)
+        crawl_style(st, base_path, "male")
         print(f"{st} completed")
