15fe635d2c72e2694522bac01b1a0a6a9b2efd8b
acensia <acensia@snu.ac.kr>
2023-09-07 15:37:17 +0900

    starting in specific point

--- add_anno.py
+++ add_anno.py
@@ -9,18 +9,26 @@ import time
 
 
 # base_path, gender, style, number
-set_dirs = glob.glob(os.path.join("./added", "*", "*", "*"))
+set_dirs = glob.glob(os.path.join("./added", "male", "*", "*"))
 
 driver = webdriver.Chrome()
 
 for folder in set_dirs:
     with open(f"{folder}/anno.json", 'r', encoding='utf-8') as j:
         anno_d = json.load(j)
+    fold = folder.split("\\")
+    # print(fold)
+    print(f"{fold[1]}, {fold[2]} : {fold[3]}")
+    # if fold[1] == 'male' and fold[2] == 'casual' and int(fold[3][0]) < 6:
+    #     continue
     for i_d in anno_d["items"]:
         single_url = i_d["link"]
         driver.get(single_url)
+        time.sleep(0.4)
         texts = driver.find_elements(By.XPATH, '//p[@class="item_categories"]/a')
         i_d["type"] = texts[0].text
+        if len(texts) < 2:
+            continue
         i_d["label"] = texts[1].text
         
     with open(f"{folder}/anno_added.json", 'w', encoding='utf-8') as j:
